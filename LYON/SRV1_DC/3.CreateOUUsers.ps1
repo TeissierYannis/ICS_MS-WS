8325f4e65915cbd60381e53bafd06222d73b7d18
Teissier Yannis <yannis.teissier70@gmail.com>
2022-03-11 14:53:41 +0100

    Update and rename 3. Create OU And Users.ps1 to 3.CreateOUUsers.ps1

--- LYON/SRV1_DC/3.CreateOUUsers.ps1
+++ LYON/SRV1_DC/3.CreateOUUsers.ps1
@@ -1,4 +1,61 @@
 # new random user
+[CmdletBinding()]
+    param (
+    [Parameter(Position = 0)]
+    [ValidateRange(1,500)]
+    [int] $Amount,
+    
+    [Parameter()]
+    [ValidateSet('Male','Female')]
+    [string] $Gender,
+    
+    # Supported nationalities: AU, BR, CA, CH, DE, DK, ES, FI, FR, GB, IE, IR, NL, NZ, TR, US
+    [Parameter()]
+    [string[]] $Nationality,
+    
+    
+    [Parameter()]
+    [ValidateSet('json','csv','xml')]
+    [string] $Format = 'json',
+    
+    # Fields to include in the results.
+    # Supported values: gender, name, location, email, login, registered, dob, phone, cell, id, picture, nat
+    [Parameter()]
+    [string[]] $IncludeFields,
+    
+    # Fields to exclude from the the results.
+    # Supported values: gender, name, location, email, login, registered, dob, phone, cell, id, picture, nat
+    [Parameter()]
+    [string[]] $ExcludeFields
+    )
+    
+    $rootUrl = "http://api.randomuser.me/?format=$($Format)"
+    
+    if ($Amount) {
+    $rootUrl += "&results=$($Amount)"
+    }
+    
+    if ($Gender) {
+    $rootUrl += "&gender=$($Gender)"
+    }
+    
+    
+    if ($Nationality) {
+    $rootUrl += "&nat=$($Nationality -join ',')"
+    }
+    
+    if ($IncludeFields) {
+    $rootUrl += "&inc=$($IncludeFields -join ',')"
+    }
+    
+    if ($ExcludeFields) {
+    $rootUrl += "&exc=$($ExcludeFields -join ',')"
+    }
+    
+    Invoke-RestMethod -Uri $rootUrl
+    }
+
+#   $tt= New-RandomUser -Amount 35 -Nationality FR -IncludeFields name,dob,phone,cell -ExcludeFields picture | Select-Object -ExpandProperty results
 
 # OU
 $OUS = "Lyon", "Lyon/Direction", "Lyon/Informatique", "Lyon/Comptabilite", "Lyon/Marketing", "Lyon/Production", "Lyon/Clients", "Paris", "Paris/Direction", "Paris/Informatique", "Paris/Comptabilite", "Paris/Marketing", "Paris/Production", "Paris/Clients"
