a2b713f090449d3013f7f9d91c5b5a2147a25785
Teissier Yannis <yannis.teissier70@gmail.com>
2022-03-11 18:01:43 +0100

    err

--- PARIS/SRV1_P/1.Setup.ps1
+++ PARIS/SRV1_P/1.Setup.ps1
@@ -5,7 +5,7 @@ $Prefix = "24"
 $Gateway = "172.31.2.254"
 $DNS = "1172.31.1.1"
 
-Rename-Computer -NewName $NameComputer -Force
+Rename-Computer -NewName $Name -Force
 
 New-NetIPAddress -IPAddress $IP -PrefixLength $Prefix -InterfaceIndex (Get-NetAdapter).ifIndex -DefaultGateway $Gateway
 Set-DnsClientServerAddress -InterfaceIndex (Get-NetAdapter).ifIndex -ServerAddresses ($DNS)
