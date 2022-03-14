2409c66fc2c55f4d40565125a5b12b03fa6f1abd
Teissier Yannis <yannis.teissier70@gmail.com>
2022-03-14 13:27:22 +0100

    Groups u^date

--- LYON/SRV1_DC/4.Groups.ps1
+++ LYON/SRV1_DC/4.Groups.ps1
@@ -1,3 +1,9 @@
+# Create "Jean Vien" & "Jean Peuplus"
+New-ADUser -Name "Jean Vien" -SamAccountName "JVien" -CannotChangePassword:$true -Path "DC=ESN,DC=dom"
+New-ADUser -Name "Jean Peuplus" -SamAccountName "JPeuplus" -CannotChangePassword:$true -Path "DC=ESN,DC=dom"
+New-LocalGroup -Name "GL_ESN"
+Add-LocalGroupMember -Group "GL_ESN" -Member "Jean Vien", "Jean Peuplus"
+
 # Create groups group
 New-ADOrganizationalUnit -Name 'Groups' -Path "DC=ESN,DC=dom"
 $filter = 'Name -like "*Groups*"'
@@ -12,7 +18,7 @@ $filter = 'Name -like "*Local*"'
 Get-ADOrganizationalUnit -Filter $filter | Set-ADObject -ProtectedFromAccidentalDeletion:$false -PassThru
 
 $GG = "Responsables:title|Responsable", "Employe:title|Responsable,Employe", "Informatique:Department|Informatique", "Comptabilite:Deparment|Comptabilite", "Direction:Department|Direction", "Marketing:Department|Marketing", "Production:Department|Production"
-$GL = ""
+$GL = "DL_Total_Ressources:Responsables,Direction", "DL_Lecture_Ressources:Informatique,Comptabilite,Marketing", "DL_Refuser_Ressources:Production"
 
 foreach ($G in $GG) {
     $splitted = $G.Split(':')
@@ -25,6 +31,21 @@ foreach ($G in $GG) {
 
     foreach($v in $values) {
         $filter = $field + ' -eq "' + $v + '" -and department -ne "Clients"'
+        if ($group -eq "Clients") {
+            $filter = $field + ' -eq "' + $v + '"'
+        }
         Get-ADUser -Filter $filter | Add-ADPrincipalGroupMembership -MemberOf $group 
     }
 }
+
+foreach ($G in $GL) {
+    $splitted = $G.Split(':')
+    $group = $splitted[0]
+    $path = "OU=Global,OU=Groups,DC=ESN,DC=dom"
+    New-ADGroup -name $group -GroupScope DomainLocal -Path $path
+    foreach($v in $splitted[1].Split(',')) {
+        $filter = 'Name -eq "' + $v + '"'
+        Get-ADGroup -Filter $filter | Add-ADPrincipalGroupMembership -MemberOf $group
+    }
+}
+Get-ADGroupMember Responsables | Get-ADUser -Properies Name,Department | Where {$_.Department -eq "Informatique"} | Add-ADPrincipalGroupMembership -MemberOf "Domain Admins"
